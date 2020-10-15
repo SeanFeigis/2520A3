e27dcaa8be0dcfb43d00f9f1c58a308456e38e47
socs <socs@socsvm.local>
2020-10-14 23:09:36 -0400

    80% commit

---
+++ list.c
@@ -0,0 +1,140 @@
+#include "list.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Performance *newPerformance() {
+  struct Performance *perf;
+  if ((perf = malloc(sizeof(struct Performance))) == NULL) {
+    fprintf(stderr, "%s\n", "Error, malloc failed");
+  }
+  perf->reads=0;
+  perf->writes=0;
+  perf->mallocs=0;
+  perf->frees=0;
+  return perf;
+}
+
+void push (struct Performance *performance, struct Node **list_ptr, void *src, unsigned int width) {
+  struct Node *temp = (struct Node *) malloc(sizeof(struct Node));
+  if (temp == NULL) {
+    fprintf(stderr, "%s\n", "Error, malloc failed");
+    exit(0);
+  }
+  if ((temp->data = (void *)malloc(sizeof(width))) == NULL) {
+    fprintf(stderr, "%s\n", "Error, malloc failed");
+    exit(0);
+  }
+
+  memcpy((char *)temp->data,src,width);
+
+  temp->next = *list_ptr;
+  *list_ptr = temp;
+
+  performance->mallocs++;
+  performance->writes++;
+}
+
+
+void readHead (struct Performance *performance, struct Node **list_ptr, void *dest, unsigned int width ) {
+  if (*list_ptr != NULL) {
+    memcpy(dest,(char *) (*list_ptr)->data, width);
+    performance->reads++;
+  } else {
+    fprintf(stderr, "%s\n", "Error, list empty");
+    exit(0);
+  }
+}
+
+void pop (struct Performance *performance, struct Node **list_ptr, void *dest, unsigned int width ) {
+  if (*list_ptr == NULL) {
+    fprintf(stderr, "%s\n", "Error, list empty");
+    exit(0);
+  }
+  memcpy(dest, (*list_ptr)->data, width);
+  struct Node *temp = *list_ptr;
+  *list_ptr = (*list_ptr)->next;
+  free(temp->data);
+  free(temp);
+
+  performance->frees++;
+  performance->reads++;
+
+}
+
+struct Node **next( struct Performance *performance, struct Node **list_ptr) {
+  if (*list_ptr == NULL) {
+    fprintf(stderr, "%s\n", "Error, list empty");
+    exit(0);
+  }
+  performance->reads++;
+  return &((*list_ptr)->next);
+}
+
+int isEmpty (struct Performance *performance, struct Node **list_ptr) {
+  if (*list_ptr == NULL) {
+    //printf("%s\n", "blah2");
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+void freeList(struct Performance *performance, struct Node **list_ptr) {
+  void *tempDest = malloc(sizeof(struct Node));
+  while (!(isEmpty(performance, list_ptr))) {
+    pop(performance, list_ptr, tempDest, sizeof((*list_ptr)->data));
+  }
+  free(tempDest);
+}
+
+void readItem( struct Performance *performance, struct Node **list_ptr, unsigned int index, void *dest, unsigned int width) {
+  struct Node **temp = list_ptr;
+  for(int i = 0; i < index ; i++) {
+    //printf("Data: %p\n", temp->data );
+    temp = next(performance, temp);
+    //printf("Data: %p\n", temp->data );
+  }
+  //printf("Data: %p\n", temp->data );
+  readHead(performance, temp, dest, width);
+}
+
+
+void appendItem( struct Performance *performance, struct Node **list_ptr, void *src, unsigned int width) {
+  struct Node **temp = list_ptr;
+  while(!(isEmpty(performance, temp))) {
+    //printf("%s\n", "blah");
+    temp = next(performance, temp);
+    //printf("%s\n", "blah");
+  }
+  push(performance, temp, src, width);
+  //printf("Data: %p\n", temp->data );
+}
+
+void insertItem( struct Performance *performance, struct Node **list_ptr, unsigned int index, void *src, unsigned int width) {
+  struct Node **temp = list_ptr;
+  for(int i = 0; i < index; i++) {
+    temp = next(performance, temp);
+  }
+  push(performance, temp, src, width);
+}
+
+void prependItem( struct Performance *performance, struct Node **list_ptr, void *src, unsigned int width) {
+  insertItem(performance, list_ptr, 0, src, width);
+}
+
+void deleteItem(struct Performance *performance, struct Node **list_ptr, unsigned int index) {
+  void *tempDest = malloc(sizeof(struct Node));
+  struct Node **temp = list_ptr;
+  for(int i = 0; i < index; i++) {
+    temp = next(performance, temp);
+  }
+  pop(performance, temp, tempDest, sizeof((*list_ptr)->data));
+  free(tempDest);
+}
+
+
+int findItem(struct Performance *performance, struct Node **list_ptr, int (*compar)(const void*, const void *), void *target, unsigned int width) {
+
+
+}
---
+++ makefile
@@ -0,0 +1,28 @@
+CFLAGS = -std=c99 -pedantic -Wall
+CC = gcc
+
+all: list.o list.h test1 test2
+
+list.o: list.c list.h
+	$(CC) $(CFLAGS) -c list.c
+
+test1.o: test1.c
+	$(CC) $(CFLAGS) -c test1.c -o test1.o
+
+test1: test1.o list.c list.h
+	$(CC) $(CFLAGS) test1.o list.o -o test1
+
+test2.o: test2.c
+	$(CC) $(CFLAGS) -DLIST -c test2.c -o test2.o
+
+test2: test2.o list.c list.h
+	$(CC) $(CFLAGS) -DLIST test2.o list.o -o test2
+
+test3.o: test3.c
+	$(CC) $(CFLAGS) -c test3.c -o test3.o
+
+test3: test3.o list.c list.h
+	$(CC) $(CFLAGS) test3.o list.o -o test3
+
+clean:
+	rm *.o test1 test2
